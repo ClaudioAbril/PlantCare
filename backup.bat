ea34af8b5cdece4f21e11168f52624e4c6dca2b5
Claudio <claudio@email.com>
2025-08-31 17:43:48 -0300

    Deploy automatico: Actualizacion de la app PlantCare

---
+++ backup.bat
@@ -0,0 +1,26 @@
+@echo off
+copy *.* "E:\Archivos-Personales\GoogleDrive\Codigo Propio\HTML\PlantCare\"
+copy *.* "E:\Archivos-Personales\OneDrive\Archivos Privados\Codigo Propio\HTML\PlantCare\"
+
+echo.
+echo ==================================================
+echo    Subiendo cambios a GitHub...
+echo ==================================================
+echo.
+git add index.html
+git add GEMINI-plant_identifier_app.html
+git add manifest.json
+git add sw.js
+git add backup.bat
+
+git commit -m "Deploy automatico: Actualizacion de la app PlantCare"
+REM Sube los cambios a la rama principal de tu repositorio en GitHub.
+git push origin main
+
+echo.
+echo ==================================================
+echo      PROCESO DE PUBLICACION FINALIZADO
+echo ==================================================
+echo.
+
+pause
